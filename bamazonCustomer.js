aadde88848276fbeabbdac733c978bf073c4335d
Sahra Anies <sahra.anies@gmail.com>
2019-07-14 00:57:58 -0700

    added prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,17 +10,35 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("Connected as id " + connection.threadId);
     displayItems();
+    purchase();
 });
 
 function displayItems() {
-    connection.query("Select * FROM products", function(err, res) {
+    connection.query("Select * FROM products", function (err, res) {
         if (err) throw err;
         // Log all results from products table
         console.log(res);
         connection.end();
+    });
+}
+
+function purchase() {
+    inquirer.prompt([
+        {
+            name: "id",
+            type: "input",
+            message: "Enter product ID"
+        },
+        {
+            name: "quantity",
+            type: "number",
+            message: "How many units do you wish to purchase?"
+        }
+    ]).then(function (answer) {
+        console.log("Insufficient quantity!");
     });
 }
